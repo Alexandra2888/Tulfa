8fc2172eb2cee07d06e209ee0ebbac5c41f50a44
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-11-10 18:04:40 +0200

    Adding CTO3 section.

---
+++ src/components/DescriptionCto3/DescriptionCto3.jsx
@@ -0,0 +1,24 @@
+const DescriptionCto3 = () => {
+  return (
+    <section
+      className="w-full min-h-screen flex justify-center items-center"
+      style={{
+        background:
+          "linear-gradient(113deg, #2A266A 0%, #433E99 50%, #2A266A 100%)",
+      }}
+    >
+      <div className="max-w-4xl mx-auto px-6 py-12">
+        <span className="block text-white text-5xl md:text-6xl xl:text-8xl font-semibold leading-normal">
+          Lorem ipsum dolor sit amet.
+        </span>
+        <span className="block mt-4 text-[rgba(255,255,255,0.6)] font-['SF_Pro_Display'] text-[3.125rem] font-semibold leading-normal">
+          Quo odit atque ut architecto obcaecati rem vitae tempore non
+          asperiores consequatur ut error facilis est architecto doloribus eos
+          laborum praesentium!
+        </span>
+      </div>
+    </section>
+  );
+};
+
+export default DescriptionCto3;
