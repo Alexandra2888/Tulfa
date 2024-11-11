a3488ec2fbc23604df7bd35a6bfba6618d869746
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-11-11 14:48:46 +0200

    Adding footer component.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <section>
+      <p className="text-center py-24 text-[#2A266A] mx-auto font-['SF_Pro_Display'] text-xl xl:text-[1.625rem] font-normal leading-relaxed px-12 xl:px-36 xl:max-w-6xl">
+        Lorem ipsum dolor sit amet consectetur. Augue elementum morbi in ac. Leo
+        eu elit nibh nunc vitae eget massa sed sed. Sit sed aliquam sit nulla
+        eget.
+      </p>
+    </section>
+  );
+}
+
+export default Footer
