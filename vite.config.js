43a1758858023558a752d1e573cf6e381362417d
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-11-12 22:46:40 +0200

    Hotfix: update vite.config.

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["lodash"],
-      output: {
-        globals: {
-          lodash: "_",
-        },
-      },
-    },
-  },
   resolve: {
     alias: {
-      lodash: "lodash-es", 
+      lodash: "lodash-es",
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["lodash-es"],
+      output: {
+        globals: {
+          "lodash-es": "_", 
+        },
+      },
     },
   },
   optimizeDeps: {
