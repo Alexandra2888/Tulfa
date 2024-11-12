ef9792d25b4735ac797189b0188a837cc6f280c2
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-11-12 22:59:41 +0200

    Hotfix: change lodash with framer motion.

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,6 +4,7 @@ import { useVideoScroll } from "../../hooks/useVideoScroll";
 import { useScrollAnimation } from "../../hooks/useScrollAnimation";
 import { S3_CONFIG } from "../../config/bannerConfig";
 import { BannerContent } from "./BannerContent";
+import { motion } from "framer-motion";
 
 export const Banner = () => {
   const containerRef = useRef(null);
@@ -23,8 +24,11 @@ export const Banner = () => {
   const { isLoading, useFallbackVideo, loadingProgress, loadedImages } =
     useImagePreloader(imageUrls, S3_CONFIG);
 
-  const { currentFrame, showIntroducing: imageShowIntroducing } =
-    useScrollAnimation(containerRef, S3_CONFIG.totalFrames, useFallbackVideo);
+  const {
+    currentFrame,
+    showIntroducing: imageShowIntroducing,
+    animationControls,
+  } = useScrollAnimation(containerRef, S3_CONFIG.totalFrames, useFallbackVideo);
 
   const { showIntroducing: videoShowIntroducing } = useVideoScroll(
     containerRef,
@@ -59,12 +63,18 @@ export const Banner = () => {
 
   return (
     <section className="relative w-full">
-      <div ref={containerRef} className="h-[500vh] relative">
-        <BannerContent
-          isLoading={isLoading}
-          loadingProps={loadingProps}
-          contentProps={contentProps}
-        />
+      <div ref={containerRef} className=" relative">
+        <motion.div
+          animate={animationControls}
+          initial={{ opacity: 0 }}
+          className="scroll-animation"
+        >
+          <BannerContent
+            isLoading={isLoading}
+            loadingProps={loadingProps}
+            contentProps={contentProps}
+          />
+        </motion.div>
       </div>
     </section>
   );
--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      lodash: "lodash-es",
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ["lodash-es"],
-      output: {
-        globals: {
-          "lodash-es": "_", 
-        },
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ["lodash-es"],
-  },
 });
