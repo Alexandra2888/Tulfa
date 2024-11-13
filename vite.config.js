823157df6f097bfcdd522ef52ebb74f184d9e82d
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-11-13 12:12:43 +0200

    Upfdate vite.config.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Tulfa-Test/',  
-
+  base: "/Tulfa-Test/",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
 });
