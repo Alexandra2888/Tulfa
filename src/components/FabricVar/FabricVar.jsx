136a81b3031ea501d156e479f0b1c7592c3eeb73
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-11-15 18:03:25 +0200

    Fix overlay position for FabricVar section.

--- src/components/FabricVar/FabricVar.jsx
+++ src/components/FabricVar/FabricVar.jsx
@@ -9,7 +9,7 @@ const FabricVar = () => {
   const selectedFabricData = fabrics.find((f) => f.id === selectedFabric) || fabrics[0];
 
   return (
-      <section className="mx-auto container px-4 pt-8">
+      <section className="container mx-auto px-4 pt-8">
         <div className="flex flex-col md:flex-row md:gap-12 lg:gap-24">
           {/* Couch Preview */}
           <div className="w-full md:w-1/2 flex items-center justify-center mb-8 md:mb-0">
@@ -25,9 +25,9 @@ const FabricVar = () => {
           {/* Fabric Selection */}
           <div className="w-full md:w-1/2 flex flex-col items-center md:items-start">
             {/* Fabric Detail View */}
-            <div className="relative w-screen md:w-auto">
+            <div className="relative w-full">
               <div
-                  className="h-[33.88rem] w-screen md:w-[23.82rem] xl:w-[40rem] 2xl:w-[60rem] cursor-pointer md:-mt-10 xl:-mt-12"
+                  className="h-[33.88rem] w-full overflow-hidden cursor-pointer md:-mt-10 xl:-mt-9"
                   onMouseEnter={() => setShowOptions(true)}
                   onMouseLeave={() => setShowOptions(false)}
               >
@@ -39,7 +39,13 @@ const FabricVar = () => {
 
                 {/* Fabric Options Overlay */}
                 {showOptions && (
-                    <div className="absolute -top-9 right-0 w-48 bg-white shadow-xl rounded-bl-lg">
+                    <div
+                        className={`absolute bg-white shadow-xl
+                    ${window.innerWidth < 768
+                            ? 'bottom-0 left-0 right-0 rounded-t-lg'
+                            : 'w-48 -top-5 xl:-top-9 right-0 rounded-bl-lg'
+                        }`}
+                    >
                       <div className="flex flex-col">
                         {fabrics.map((fabric) => (
                             <Button
